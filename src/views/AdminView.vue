545c282ae23de744e466dcd4106d71f6d72fdd41
許 朝智 <georgehsu1110@gmail.com>
2025-12-17 15:35:12 +0800

    1.0.0

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -177,7 +177,7 @@ onMounted(() => {
               v-model="editingShop.phone"
               type="text" 
               class="w-full bg-slate-900/50 border border-slate-600 rounded-lg px-4 py-2.5 text-white placeholder-slate-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all" 
-              placeholder="02-xxxx-xxxx"
+              placeholder="03-xxxx-xxxx"
             >
           </div>
 
